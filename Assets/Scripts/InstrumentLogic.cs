4ac81734b52fd527eb52a6612138d7febf38fcc2
adamblin <abenitez@edu.tecnocampus.cat>
2023-12-30 00:08:00 +0100

    Cambios

--- Assets/Scripts/InstrumentLogic.cs
+++ Assets/Scripts/InstrumentLogic.cs
@@ -7,12 +7,18 @@ public class InstrumentLogic : MonoBehaviour
 {
     public static Action<GameObject> onInstrument;
     public Color intrumentUIColor;
-
+    public GameObject light;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag.Equals("Player")) {
             Debug.Log("Instrument Collected");
             onInstrument?.Invoke(this.gameObject);
+            ActivateLight();
         }
+    }
+
+    private void ActivateLight()
+    {
+        light.active = true;
     }
 }
