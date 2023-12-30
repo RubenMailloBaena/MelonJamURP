2b37d56e9de69378d08dde246425fa23e683e215
RubenMailloBaena <rubenmaillo2003@gmail.com>
2023-12-30 16:01:28 +0100

    sadsd

--- Assets/Scripts/EndLevelPanel.cs
+++ Assets/Scripts/EndLevelPanel.cs
@@ -4,13 +4,7 @@ using UnityEngine;
 
 public class EndLevelPanel : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
     void Update()
     {
         
