8166e7f2c81a6f4fec0cf0f868dfc81521a7df68
adamblin <abenitez@edu.tecnocampus.cat>
2023-12-30 12:11:44 +0100

    Mas cosas

--- Assets/Scripts/InstrumentLogic.cs
+++ Assets/Scripts/InstrumentLogic.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
+using UnityEngine.Rendering.Universal;
+
 public class InstrumentLogic : MonoBehaviour
 {
     public static Action<GameObject> onInstrument;
     public Color intrumentUIColor;
     public GameObject lightPrincipal;
-    
-    
-   
+    public Light2D smallLight;
+    [SerializeField]
+    private Color smallColor;
+
+    private void Start()
+    {
+        smallLight = GameObject.Find("FormLight").GetComponent<Light2D>();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag.Equals("Player")) {
@@ -24,6 +32,6 @@ public class InstrumentLogic : MonoBehaviour
     {
         lightPrincipal.active = true;
         gameObject.GetComponent<SpriteRenderer>().color=Color.white;
-       
+        smallLight.color = smallColor;
     }
 }
